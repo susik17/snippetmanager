a92c8d5d54bd5aaf5f4acf2dcc5a66c3b239b415
susik17 <susikumar1718@gmail.com>
2025-08-24 22:16:01 +0530

    Initial commit of Code Snippet Manager project

---
+++ src/main/java/com/example/snippetmanager/SnippetmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.snippetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippetmanagerApplication.class, args);
+	}
+
+}
